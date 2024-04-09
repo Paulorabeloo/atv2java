068329279bc369373bbc98d8ef70e939607ea205
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-04-09 13:29:29 -0300

    aula1ex3

---
+++ Aula1ex3.java
@@ -0,0 +1,19 @@
+package com.mycompany.aula1ex3;
+
+public class Aula1ex3 
+{
+    public static void main(String[] args) 
+    {
+        int a = 100, b = 50, soma, produto, razao; // declarando as varíaveis 
+    // a e b inicializadas com 0
+  
+    soma = (a + b);                          // operação soma
+    System.out.println("Resultado: " + soma); // resultado na tela
+    
+    produto = (a * b);                         // operação produto
+    System.out.println("Resultado: " + produto); // resultado na tela
+    
+    razao = (a / b);                            // operação razão
+    System.out.println("Resultado: " + razao); // resultado na tela
+    }
+}
